241263839dd96a424fb42d38421e7b9f6f2c1abc
Sean Moss <moss.seank@gmail.com>
2020-10-02 10:01:51 -0400

    VulkanInstance creation, physical device enumeration

--- VVK/VulkanInstance.cs
+++ VVK/VulkanInstance.cs
@@ -5,6 +5,9 @@
  */
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace VVK
 {
@@ -13,6 +16,19 @@ namespace VVK
 	/// </summary>
 	public unsafe sealed partial class VulkanInstance : IDisposable
 	{
+		#region Static Fields
+		/// <summary>
+		/// A list of instance extensions that are supported by the runtime.
+		/// </summary>
+		public static IReadOnlyList<string> Extensions => _Extensions ?? PopulateExtensions();
+		private static List<string>? _Extensions = null;
+		/// <summary>
+		/// A list of layers available on the runtime.
+		/// </summary>
+		public static IReadOnlyList<string> Layers => _Layers ?? PopulateLayers();
+		private static List<string>? _Layers = null;
+		#endregion // Static Fields
+
 		#region Fields
 		/// <summary>
 		/// The VkInstance handle wrapped by this object.
@@ -22,6 +38,26 @@ namespace VVK
 		/// The table of loaded functions for the instance.
 		/// </summary>
 		public Vk.InstanceFunctionTable Functions { get; private set; }
+
+		/// <summary>
+		/// List of the physical devices on the system available to the instance.
+		/// </summary>
+		public IReadOnlyList<VulkanPhysicalDevice> PhysicalDevices => _devices;
+		private readonly List<VulkanPhysicalDevice> _devices = new();
+
+		/// <summary>
+		/// Gets if the instance has the VK_EXT_debug_utils extension enabled.
+		/// </summary>
+		public bool HasDebugUtils => _debugHandle;
+		private Vk.EXT.DebugUtilsMessenger _debugHandle = Vk.EXT.DebugUtilsMessenger.Null;
+		private int* _debugToken; // Token for dispatching util messages
+		/// <summary>
+		/// Event that is called when a message is received from VK_EXT_debug_utils.
+		/// </summary>
+		public event DebugUtilsCallback? OnDebugUtilMessage = null;
+
+		// Static dispatch dictionary
+		private static readonly List<VulkanInstance?> _DebugDispatch = new();
 		#endregion // Fields
 
 		/// <summary>
@@ -32,6 +68,38 @@ namespace VVK
 		{
 			Handle = inst;
 			Functions = new(inst);
+
+			// Enumerate the physical devices
+			uint count = 0;
+			EnumeratePhysicalDevices(&count, null).Throw();
+			var devices = stackalloc Vk.PhysicalDevice[(int)count];
+			EnumeratePhysicalDevices(&count, devices).Throw();
+			for (uint i = 0; i < count; ++i) {
+				_devices.Add(new VulkanPhysicalDevice(this, devices[i]));
+			}
+
+			// Enable debug utils if available
+			if (Functions.vkCreateDebugUtilsMessengerEXT != null) {
+				// Setup dispatch
+				_debugToken = (int*)Marshal.AllocHGlobal(sizeof(int)).ToPointer();
+				*_debugToken = _DebugDispatch.Count;
+				_DebugDispatch.Add(this);
+
+				// Create the object
+				Vk.EXT.DebugUtilsMessengerCreateInfo.New(out var mci);
+				mci.Flags = Vk.EXT.DebugUtilsMessengerCreateFlags.NoFlags;
+				mci.MessageSeverity =
+					Vk.EXT.DebugUtilsMessageSeverityFlags.ErrorEXT | Vk.EXT.DebugUtilsMessageSeverityFlags.WarningEXT | 
+					Vk.EXT.DebugUtilsMessageSeverityFlags.InfoEXT | Vk.EXT.DebugUtilsMessageSeverityFlags.VerboseEXT;
+				mci.MessageType = 
+					Vk.EXT.DebugUtilsMessageTypeFlags.GeneralEXT | Vk.EXT.DebugUtilsMessageTypeFlags.PerformanceEXT | 
+					Vk.EXT.DebugUtilsMessageTypeFlags.ValidationEXT;
+				mci.UserCallback = &_DebugUtilsCallback;
+				mci.UserData = _debugToken;
+				Vk.EXT.DebugUtilsMessenger debugHandle;
+				CreateDebugUtilsMessengerEXT(&mci, null, &debugHandle).Throw();
+				_debugHandle = debugHandle;
+			}
 		}
 		~VulkanInstance()
 		{
@@ -50,12 +118,145 @@ namespace VVK
 
 		private void dispose(bool disposing)
 		{
-			if (Handle) {
-				Functions.vkDestroyInstance(Handle, null);
+			if (_debugHandle) {
+				DestroyDebugUtilsMessengerEXT(_debugHandle, null);
+				_DebugDispatch[*_debugToken] = null;
+				Marshal.FreeHGlobal(new IntPtr(_debugToken));
+				_debugHandle = Vk.EXT.DebugUtilsMessenger.Null;
+				_debugToken = null;
 			}
-			Handle = Vk.Instance.Null;
-			Functions = new();
+			if (Handle) {
+				DestroyInstance(null);
+				Handle = Vk.Instance.Null;
+				Functions = new();
+				_devices.Clear();
+			}
 		}
 		#endregion // IDisposable
+
+		/// <summary>
+		/// Creates a new Vulkan instance from the passed information.
+		/// </summary>
+		/// <param name="appName">The name of the application.</param>
+		/// <param name="appVersion">The version of the application.</param>
+		/// <param name="apiVersion">The minimum required version for the Vulkan API.</param>
+		/// <param name="extensions">
+		/// The list of extensions to enable. An exception will be thrown for extensions that are not available. Check
+		/// <see cref="VulkanInstance.Extensions"/> to see the available functions.
+		/// </param>
+		/// <param name="layers">
+		/// The list of layers to enable. An exception will be thrown for layers that are not available. Check
+		/// <see cref="VulkanInstance.Layers"/> to see the available layers.
+		/// </param>
+		/// <returns>The new Vulkan instance object.</returns>
+		/// <exception cref="VulkanResultException">One of the Vulkan API calls failed.</exception>
+		/// <exception cref="PlatformNotSupportedException">The API version, extension, or layer is not supported.</exception>
+		public static VulkanInstance Create(string appName, Vk.Version appVersion, Vk.Version apiVersion, 
+			IEnumerable<string>? extensions = null, IEnumerable<string>? layers = null)
+		{
+			// Check instance version
+			Vk.Version vers;
+			EnumerateInstanceVersion(&vers.Value).Throw();
+			if (vers < apiVersion) {
+				throw new PlatformNotSupportedException(
+					$"Supported API version ({vers}) is less than requested ({apiVersion})");
+			}
+
+			// Check extensions
+			if (extensions?.FirstOrDefault(s => !Extensions.Contains(s)) is string extMiss) {
+				throw new PlatformNotSupportedException($"The instance extension '{extMiss}' is not supported");
+			}
+			if (layers?.FirstOrDefault(s => !Layers.Contains(s)) is string layerMiss) {
+				throw new PlatformNotSupportedException($"The debug layer '{layerMiss}' is not supported");
+			}
+
+			// Convert to native strings
+			using var extNames = new NativeStringList(extensions ?? Enumerable.Empty<string>());
+			using var layerNames = new NativeStringList(layers ?? Enumerable.Empty<string>());
+			using var appNameStr = new NativeString(appName);
+			using var engNameStr = new NativeString("VVK");
+
+			// Application info
+			var vvkv = typeof(VulkanInstance).Assembly.GetName().Version!;
+			Vk.ApplicationInfo.New(out var appi);
+			appi.ApplicationName = appNameStr.Data;
+			appi.ApplicationVersion = appVersion;
+			appi.EngineName = engNameStr.Data;
+			appi.EngineVersion = new Vk.Version((uint)vvkv.Major, (uint)vvkv.Minor, (uint)vvkv.Revision);
+			appi.ApiVersion = apiVersion;
+
+			// Instance create
+			Vk.InstanceCreateInfo.New(out var ici);
+			ici.ApplicationInfo = &appi;
+			ici.EnabledLayerCount = layerNames.Count;
+			ici.EnabledLayerNames = layerNames.Data;
+			ici.EnabledExtensionCount = extNames.Count;
+			ici.EnabledExtensionNames = extNames.Data;
+			Vk.Instance handle;
+			CreateInstance(&ici, null, &handle).Throw();
+
+			// Return
+			return new(handle);
+		}
+
+		// Callback function for the debug utils messenger object
+		private static Vk.Bool32 _DebugUtilsCallback(
+			Vk.EXT.DebugUtilsMessageSeverityFlags severity,
+			Vk.EXT.DebugUtilsMessageTypeFlags type,
+			Vk.EXT.DebugUtilsMessengerCallbackData* data,
+			void* userData
+		)
+		{
+			_DebugDispatch[*(int*)userData]?.OnDebugUtilMessage?.Invoke(severity, type, data);
+			return Vk.Bool32.False;
+		}
+
+		private static List<string> PopulateExtensions()
+		{
+			_Extensions = new();
+
+			// Get extension properties
+			uint count = 0;
+			EnumerateInstanceExtensionProperties(null, &count, null).Throw();
+			var exts = stackalloc Vk.ExtensionProperties[(int)count];
+			EnumerateInstanceExtensionProperties(null, &count, exts).Throw();
+
+			// Convert to string list
+			for (int i = 0; i < count; ++i) {
+				_Extensions.Add(exts[i].ExtensionName);
+			}
+
+			return _Extensions;
+		}
+
+		private static List<string> PopulateLayers()
+		{
+			_Layers = new();
+
+			// Get layer properties
+			uint count = 0;
+			EnumerateInstanceLayerProperties(&count, null).Throw();
+			var layers = stackalloc Vk.LayerProperties[(int)count];
+			EnumerateInstanceLayerProperties(&count, layers).Throw();
+
+			// Convert to string list
+			for (int i = 0; i < count; ++i) {
+				_Layers.Add(layers[i].LayerName);
+			}
+
+			return _Layers;
+		}
 	}
+
+	/// <summary>
+	/// Callback for messages from VK_EXT_debug_utils.
+	/// </summary>
+	/// <param name="severity">The message severity.</param>
+	/// <param name="type">The message type.</param>
+	/// <param name="data">The message data.</param>
+	public unsafe delegate void DebugUtilsCallback(
+		Vk.EXT.DebugUtilsMessageSeverityFlags severity,
+		Vk.EXT.DebugUtilsMessageTypeFlags type,
+		Vk.EXT.DebugUtilsMessengerCallbackData* data
+	);
 }
--- VVK/VulkanPhysicalDevice.cs
+++ VVK/VulkanPhysicalDevice.cs
@@ -23,6 +23,56 @@ namespace VVK
 		/// The API handle for this physical device.
 		/// </summary>
 		public readonly Vk.PhysicalDevice Handle;
+
+		/// <summary>
+		/// The properties for the physical device.
+		/// </summary>
+		public readonly Vk.PhysicalDeviceProperties Properties;
+		/// <summary>
+		/// The core Vulkan 1.1 properties for the physical device.
+		/// </summary>
+		public readonly Vk.PhysicalDeviceVulkan11Properties Properties11;
+		/// <summary>
+		/// The core Vulkan 1.2 properties for the physical device.
+		/// </summary>
+		public readonly Vk.PhysicalDeviceVulkan12Properties Properties12;
+		/// <summary>
+		/// The features for the physical device.
+		/// </summary>
+		public readonly Vk.PhysicalDeviceFeatures Features;
+		/// <summary>
+		/// The core Vulkan 1.1 features for the physical device.
+		/// </summary>
+		public readonly Vk.PhysicalDeviceVulkan11Features Features11;
+		/// <summary>
+		/// The core Vulkan 1.2 features for the physical device.
+		/// </summary>
+		public readonly Vk.PhysicalDeviceVulkan12Features Features12;
+		/// <summary>
+		/// The memory properties for the physical device.
+		/// </summary>
+		public readonly Vk.PhysicalDeviceMemoryProperties MemoryProperties;
+
+		/// <summary>
+		/// The name of the device.
+		/// </summary>
+		public string Name => Properties.DeviceName;
+		/// <summary>
+		/// The device type.
+		/// </summary>
+		public Vk.PhysicalDeviceType Type => Properties.DeviceType;
+		/// <summary>
+		/// The maximum Vulkan API version supported by the device.
+		/// </summary>
+		public Vk.Version ApiVersion => new(Properties.ApiVersion);
+		/// <summary>
+		/// The version of the driver.
+		/// </summary>
+		public Vk.Version DriverVersion => new(Properties.DriverVersion);
+		/// <summary>
+		/// The vendor for the physical device.
+		/// </summary>
+		public uint VendorId => Properties.VendorID;
 		#endregion // Fields
 
 		/// <summary>
@@ -34,6 +84,101 @@ namespace VVK
 		{
 			Parent = parent.Handle ? parent : throw new ArgumentNullException(nameof(parent), "Null parent instance");
 			Handle = handle ? handle : throw new ArgumentNullException(nameof(handle), "Null physical device handle");
+
+			// Get the features/properties
+			GetDeviceProperties(this, out Properties, out Properties11, out Properties12);
+			GetDeviceFeatures(this, out Features, out Features11, out Features12);
+			Vk.PhysicalDeviceMemoryProperties memProps;
+			GetMemoryProperties(&memProps);
+			MemoryProperties = memProps;
+		}
+
+		/// <summary>
+		/// Searches for a memory type that matches the given requirements, returns <c>null</c> if no type was found.
+		/// </summary>
+		/// <param name="mask">
+		/// The type requirements mask taken from <see cref="Vk.MemoryRequirements.MemoryTypeBits"/>.
+		/// </param>
+		/// <param name="required">The memory flags required for the memory type.</param>
+		/// <param name="preferred">The memory flags that are preferred for the memory type.</param>
+		/// <param name="not">The flags that are preferred to not be present for the memory type.</param>
+		public uint? FindMemoryType(uint mask, Vk.MemoryPropertyFlags required, Vk.MemoryPropertyFlags preferred,
+			Vk.MemoryPropertyFlags not)
+		{
+			fixed (Vk.MemoryType* typePtr = &MemoryProperties.MemoryTypes_0) {
+				foreach (var notMask in new[] { not, Vk.MemoryPropertyFlags.NoFlags }) {
+					foreach (var yesMask in new[] { required | preferred, required }) {
+						for (uint mi = 0, mbit = 1; mi < MemoryProperties.MemoryTypeCount; ++mi, mbit <<= 1) {
+							var maskPass = (mask & mbit) != 0;
+							var yesPass = (typePtr[mi].PropertyFlags & yesMask) == yesMask;
+							var notPass = (typePtr[mi].PropertyFlags & notMask) == 0;
+							if (maskPass && yesPass && notPass) {
+								return mi;
+							}
+						}
+					}
+				}
+			}
+
+			return null;
+		}
+
+		private static void GetDeviceProperties(
+			VulkanPhysicalDevice device,
+			out Vk.PhysicalDeviceProperties props10,
+			out Vk.PhysicalDeviceVulkan11Properties props11,
+			out Vk.PhysicalDeviceVulkan12Properties props12
+		)
+		{
+			// Can only use properties2 on Vulkan >= 1.1
+			if (device.ApiVersion < Vk.Version.VK_VERSION_1_1) {
+				Vk.PhysicalDeviceProperties props;
+				device.GetProperties(&props);
+				props10 = props;
+				Vk.PhysicalDeviceVulkan11Properties.New(out props11);
+				Vk.PhysicalDeviceVulkan12Properties.New(out props12);
+			}
+			else {
+				Vk.PhysicalDeviceProperties2.New(out var p10);
+				Vk.PhysicalDeviceVulkan11Properties.New(out var p11);
+				Vk.PhysicalDeviceVulkan12Properties.New(out var p12);
+				p10.pNext = &p11;
+				p11.pNext = &p12;
+				device.GetProperties2(&p10);
+				props10 = p10.Properties;
+				props11 = p11;
+				props12 = p12;
+				props11.pNext = null;
+			}
+		}
+
+		private static void GetDeviceFeatures(
+			VulkanPhysicalDevice device,
+			out Vk.PhysicalDeviceFeatures feats10,
+			out Vk.PhysicalDeviceVulkan11Features feats11,
+			out Vk.PhysicalDeviceVulkan12Features feats12
+		)
+		{
+			// Can only use features2 on Vulkan >= 1.1
+			if (device.ApiVersion < Vk.Version.VK_VERSION_1_1) {
+				Vk.PhysicalDeviceFeatures feats;
+				device.GetFeatures(&feats);
+				feats10 = feats;
+				Vk.PhysicalDeviceVulkan11Features.New(out feats11);
+				Vk.PhysicalDeviceVulkan12Features.New(out feats12);
+			}
+			else {
+				Vk.PhysicalDeviceFeatures2.New(out var f10);
+				Vk.PhysicalDeviceVulkan11Features.New(out var f11);
+				Vk.PhysicalDeviceVulkan12Features.New(out var f12);
+				f10.pNext = &f11;
+				f11.pNext = &f12;
+				device.GetFeatures2(&f10);
+				feats10 = f10.Features;
+				feats11 = f11;
+				feats12 = f12;
+				feats11.pNext = null;
+			}
 		}
 	}
 }
